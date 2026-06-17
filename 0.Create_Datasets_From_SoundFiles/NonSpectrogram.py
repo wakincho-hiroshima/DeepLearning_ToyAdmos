db9a46039720816597c9343195a896d364a79266
wakincho <rintarouwaki54@gmail.com>
2026-06-18 03:29:20 +0900

    edit train file

--- 0.Create_Datasets_From_SoundFiles/NonSpectrogram.py
+++ 0.Create_Datasets_From_SoundFiles/NonSpectrogram.py
@@ -1,5 +1,3 @@
-#音を学習させるときはdB変換してからのほうがよさそう
-
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
@@ -10,18 +8,16 @@ ch_num = "1"
 BASE_AUDIO_FILE_PATH = f"/run/media/wakincho/SSPQ-USC/Projects/Datasets/toy_admos_project/Training_SoundFiles/ToyCar/ch{ch_num}/train_normal/"
 MUSIC_PATH = f"0{ch_num}0001_ToyCar_case1_normal_IND_ch{ch_num}_0001.wav"
 
-SAVE_PATH = f"/home/wakincho/Projects/DeepLearning_Projects/src_ToyAdmos/0.Create_Datasets_From_SoundFiles/Wave_png/Spectrogram_0{ch_num}0001_ToyCar_case1_normal_IND_ch{ch_num}_0001.png"
+SAVE_PATH = f"/home/wakincho/Projects/DeepLearning_Projects/src_ToyAdmos/0.Create_Datasets_From_SoundFiles/Wave_png/Nonstft_Spectrogram_0{ch_num}0001_ToyCar_case1_normal_IND_ch{ch_num}_0001.png"
 
 data, sample_rate = librosa.load(BASE_AUDIO_FILE_PATH + MUSIC_PATH)
 
-#stftで周波数解析
-stft_data = librosa.stft(data)
-
-spectrogram_dB = librosa.amplitude_to_db(np.abs(stft_data), ref=np.max)
+Spectrogram = librosa.feature.melspectrogram(y=data, sr=sample_rate)
+#Spectrogram_dB = librosa.power_to_db(Spectrogram)
 
 plt.figure(figsize=(10,4))
 
-librosa.display.specshow(spectrogram_dB, sr=sample_rate, x_axis='time', y_axis='log')
+librosa.display.specshow(Spectrogram, sr=sample_rate, x_axis='time', y_axis='log')
 
 plt.xlim(0, 11)
 
@@ -32,4 +28,3 @@ plt.ylabel("Frequency [Hz]")
 
 plt.savefig(SAVE_PATH, dpi=300)
 plt.close()
-
--- 1.Train/train_test.py
+++ 1.Train/train_test.py
@@ -1,15 +1,74 @@
 import os
-import pandas as pd
+import pandas
+
+import glob
+
 from tqdm import tqdm
+
 import librosa
-import librosa.display # インポートしないでlibrosa.display(〜〜)で実行しようとするとエラーになりました
-import matplotlib.pyplot as plt
-import IPython.display as ipd
-import seaborn as sns
+import librosa.display
+
+import matplotlib.pyplot
+
 import numpy as np
+
 from sklearn.model_selection import train_test_split
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+
+ch_num = 1
+
+config = {
+    "ch_num": ch_num,
+    "Sound_folder_dir" : f"/run/media/wakincho/SSPQ-USC/Projects/Datasets/toy_admos_project/Training_SoundFiles/ToyCar/{ch_num}/train_normal/"
+}
+
+# Sound_files配列へ,"Sound_folder_dir"フォルダに存在するwavファイル名を追加して,ファイル名配列を作成
+Sound_files = glob.glob(os.path.join(config["Sound_folder_dir"], ".wav"))
+
+def prepare_data():
+    # 音声データを辞書変数にぶちこむ
+    data_dictionaly = []
+
+    for sound_path in Sound_files:
+        data_dictionaly.append({"sound": sound_path})
+
+    print(f"{len(data_dictionaly)} sounds found.")
+
+# 画像をメルスペクトログラムに変換
+class DatsUnits(Dataset):
+    def __init__(self, base_path, df, in_col, out_col):
+        self.df = df
+        self.data = [] # 音源データをメルスペクトログラム(画像)に変換して格納する用
+        self.labels =[] # 各データのカテゴリ情報を格納
+        self.category_to_id = {}
+        self.id_to_catefory = {}
+        self.categories = list(sorted(df[out_col].unique())) # 正解ラベル格納用
+
+        for i,category in enumerate(self.categories):
+            self.category_to_id[category] = i
+            self.id_to_catefory[i] = category
+
+        for row in tqdm(range(len(df))):
+            row = df.iloc[row]
+            file_path = os.path.join(base_path, row[in_col])
+            waveform, sample_rate = librosa.load(file_path)
+
+            feature_melspec = librosa.feature.melspectrogram(y = waveform, sr=sample_rate)
+            feature_melspec_db = librosa.power_to_db(feature_melspec, ref=np.max)
+
+            self.data.append(feature_melspec_db)
+            self.labels.append(self.category_to_id[row['catefory']])
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+    
+# 学習データと検証用データに分割
+train_df, test_df = train_test_split(meta_df, train_size=0.8)
